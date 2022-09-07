ccd3fb6789ee112d4b1d07a38df4de0d606e2cc5
alexkovalenco <72919350+alexkovalenco@users.noreply.github.com>
2022-09-07 11:45:14 +0300

    small changes

--- src/test/java/com/product/controller/ProductControllerTest.java
+++ src/test/java/com/product/controller/ProductControllerTest.java
@@ -1,5 +1,41 @@
 package com.product.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProductControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductController controller;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Ignore
+    @Test
+    public void findByFilter() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+
+
+        mockMvc.perform(get("/api/shop/product?nameFilter=%5EE.*%24")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
 }
