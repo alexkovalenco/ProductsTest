314b47fdcd153ebb41f05ce059602c18c725eaa9
alexkovalenco <72919350+alexkovalenco@users.noreply.github.com>
2022-09-07 22:21:07 +0300

    small changes

--- src/main/java/com/product/controller/ProductController.java
+++ src/main/java/com/product/controller/ProductController.java
@@ -26,15 +26,19 @@ public class ProductController {
         return new ResponseEntity(productService.create(productDTO), HttpStatus.CREATED);
     }
 
-    @GetMapping(path = "/product/page", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(path = "/product", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<?> productsByQuery(@RequestParam(value = "nameFilter") String nameFilter,
                                       @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
                                       @RequestParam(value = "size", required = false, defaultValue = "15") Integer size) {
         Pageable pageable = PageRequest.of(page, size);
-        return new ResponseEntity<>(productService.findAll(nameFilter, pageable).getContent(), HttpStatus.OK);
+        List<ProductEntity> content = productService.findAll(nameFilter, pageable).getContent();
+        if(content.isEmpty()){
+            return new ResponseEntity<>(content, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(content, HttpStatus.OK);
     }
 
-    @GetMapping(path = "/product", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(path = "/product/test", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<?> productsByJava(@RequestParam(value = "nameFilter") String nameFilter) {
         List<ProductEntity> products = productService.findAll(nameFilter);
         if(products.isEmpty()){
